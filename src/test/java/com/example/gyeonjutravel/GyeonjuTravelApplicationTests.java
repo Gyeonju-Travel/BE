8e87fd5e4d33b01969668ec1b185eb32bc8c7729
SungMinju <minju2928@naver.com>
2026-07-13 17:51:12 +0900

    [Refactor] 로그아웃, 회원탈퇴 Authorization 숨김

--- src/test/java/com/example/gyeonjutravel/GyeonjuTravelApplicationTests.java
+++ src/test/java/com/example/gyeonjutravel/GyeonjuTravelApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.gyeonjutravel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GyeonjuTravelApplicationTests {
 
     @Test
