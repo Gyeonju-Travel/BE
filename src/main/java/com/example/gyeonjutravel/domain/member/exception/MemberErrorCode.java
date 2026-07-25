974f9256a080e1dfc3a4909083075556fd5ca72d
SungMinju <minju2928@naver.com>
2026-07-25 23:31:16 +0900

    [Style] 유저 오류 코드 style 수정

--- src/main/java/com/example/gyeonjutravel/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/example/gyeonjutravel/domain/member/exception/MemberErrorCode.java
@@ -17,10 +17,7 @@ public enum MemberErrorCode implements BaseErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "MEMBER_401_4", "만료된 토큰입니다."),
     LOGGED_OUT_TOKEN(HttpStatus.UNAUTHORIZED, "MEMBER_401_5", "로그아웃된 토큰입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_404_1", "회원을 찾을 수 없습니다."),
-    VERIFICATION_EMAIL_SEND_FAILED(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            "MEMBER_500_1",
-            "인증번호 이메일 발송에 실패했습니다."
+    VERIFICATION_EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER_500_1", "인증번호 이메일 발송에 실패했습니다."
     );
 
     private final HttpStatus status;
