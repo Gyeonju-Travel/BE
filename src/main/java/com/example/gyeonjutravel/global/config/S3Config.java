5b39ee87fd411a0355ce1a47d698f0f91ce94b24
SungMinju <minju2928@naver.com>
2026-09-10 03:35:52 +0900

    [Feat] 임시 서명 URL을 생성하는 S3Presigner 빈 추가

--- src/main/java/com/example/gyeonjutravel/global/config/S3Config.java
+++ src/main/java/com/example/gyeonjutravel/global/config/S3Config.java
@@ -7,10 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @ConditionalOnProperty(name = "app.storage.provider", havingValue = "s3", matchIfMissing = true)
 public class S3Config {
+    @Bean
+    public S3Presigner s3Presigner(@Value("${app.storage.s3.region}") String region) {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
     @Bean
     public S3Client s3Client(@Value("${app.storage.s3.region}") String region) {
         return S3Client.builder()
