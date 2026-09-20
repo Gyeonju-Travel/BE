452b5c93b714ee20ad4ef1756a0238ce96f6a18a
SungMinju <minju2928@naver.com>
2026-09-20 23:55:13 +0900

    [Fix] 관광공사 식별자 필드 추가

--- src/main/java/com/example/gyeonjutravel/global/tmap/MatrixNode.java
+++ src/main/java/com/example/gyeonjutravel/global/tmap/MatrixNode.java
@@ -1,8 +1,7 @@
 package com.example.gyeonjutravel.global.tmap;
 
-public record MatrixNode(
-        String key,
-        double longitude,
-        double latitude
-) {
+public record MatrixNode(String key, double longitude, double latitude, String tourContentId) {
+    public MatrixNode(String key, double longitude, double latitude) {
+        this(key, longitude, latitude, null);
+    }
 }
